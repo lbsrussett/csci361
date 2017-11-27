87868812ec5f60b60e2cdb398885c9ba32c8645a
Sullivan <q51z451@msu.montana.edu>
2017-11-27 11:33:15 -0700

    updated arch proj

--- comp_arch/main.c
+++ comp_arch/main.c
@@ -14,9 +14,9 @@ extern long long int part_three();
 int main(void)
 {
 	//code for part 2
-	long long int sorted[15] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29};
-	long long int value_returned = binary_search(sorted, 15, 29);
-	printf("Result of binary search is %ld\n", value_returned);
+	//long long int sorted[15] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29};
+	//long long int value_returned = binary_search(sorted, 15, 29);
+//	printf("Result of binary search is %ld\n", value_returned);
 	/*  ******************************************
 	additional code for debugging. You can uncomment this code to test you implementation
 	assert( 1 == binary_search(sorted, 15, 1) );
@@ -29,13 +29,13 @@ int main(void)
 
 
 	//code for part 3
-	/* ******************************************
+	// ******************************************
 	// This code will call part 3. You may choose to add parameters,
 	// depending on the nature of your project
-
+	char*hworld = part_three();
     long long int returned_part3 = part_three();
     printf("Result of part_three is %ld\n", returned_part3);
-    ****************************************** */
+ //   ******************************************
 
     return 0;
 }
