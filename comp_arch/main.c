92ff08251e2ca714b64f3f4dc5c7818c8c741ceb
Sullivan <q51z451@msu.montana.edu>
2017-11-29 18:14:00 -0700

    newest version

--- comp_arch/main.c
+++ comp_arch/main.c
@@ -16,26 +16,28 @@ int main(void)
 	//code for part 2
 	//long long int sorted[15] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29};
 	//long long int value_returned = binary_search(sorted, 15, 29);
-//	printf("Result of binary search is %ld\n", value_returned);
-	/*  ******************************************
-	additional code for debugging. You can uncomment this code to test you implementation
+	//printf("Result of binary search is %ld\n", value_returned);
+	//  ******************************************
+	/*additional code for debugging. You can uncomment this code to test you implementation
 	assert( 1 == binary_search(sorted, 15, 1) );
 	assert( 3 == binary_search(sorted, 15, 3) );
 	assert( 15 == binary_search(sorted, 15, 15) );
 	assert( 23 == binary_search(sorted, 15, 23) );
 	assert( 29 == binary_search(sorted, 15, 29) );
 	assert( -1 == binary_search(sorted, 15, 35) );
-	 ****************************************** */
+	// ******************************************/
 
 
 	//code for part 3
 	// ******************************************
 	// This code will call part 3. You may choose to add parameters,
 	// depending on the nature of your project
-	//char*hworld = part_three();
-	long long int hw[11];
-    long long int  *returned_part3 = (long long int*) part_three(hw);
-    printf("Result of part_three is %ld\n", returned_part3);
+	char hw[11];
+    char  *returned_part3 = (char*) part_three(hw);
+    //printf("Result of part_three is %ld\n", returned_part3);
+    for(int i = 0; i < 12; i++) {
+    	printf("%s\n", hw[i]);
+    }
  //   ******************************************
 
     return 0;
